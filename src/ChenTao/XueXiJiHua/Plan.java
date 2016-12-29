bbffed32d11080a95388a6e5527623e02d63916d
Administrator <Administrator@c303-014>
2016-12-29 17:36:47 +0800

    学习计划java文件

---
+++ src/ChenTao/XueXiJiHua/Plan.java
@@ -0,0 +1,27 @@
+package ChenTao.XueXiJiHua;
+
+public class Plan {
+	private String plancontent;// 学习计划内容
+	private int userid;// 用户id
+
+	public Plan() {
+
+	}
+
+	public String getPlancontent() {
+		return plancontent;
+	}
+
+	public void setPlancontent(String plancontent) {
+		this.plancontent = plancontent;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+}
